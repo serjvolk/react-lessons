8a25cfd175647f196200e7f0f9deede4c705fc7c
sv0ne <serjvolk222@gmail.com>
2021-07-31 15:35:09 +0300

    31 -  onClick, ref, VirtualDOM

--- projects/01/my-app/src/components/Profile/Profile.jsx
+++ projects/01/my-app/src/components/Profile/Profile.jsx
@@ -1,9 +1,17 @@
 import css from "./Profile.module.css";
 import ProfileInfo from "./PrifileInfo/ProfileInfo";
 import Posts from "./PrifileInfo/Posts";
+import React from "react";
 
 function Profile (props) {
     let postsElements = props.posts.posts.map((post) =>  <Posts postMessage={post.msg} id={post.id}/>);
+
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text);
+    }
 
     return (
         <div className={css.profile}>
@@ -12,6 +20,10 @@ function Profile (props) {
                 <ul className={css.posts}>
                     {postsElements}
                 </ul>
+                <div className={css.addPost}>
+                    <input type="text" autocomplete="off" ref={newPostElement}/>
+                    <button onClick={addPost}>Сохранить</button>
+                </div>
             </div>
         </div>
     );
