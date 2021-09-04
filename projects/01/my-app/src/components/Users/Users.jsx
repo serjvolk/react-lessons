be2bcb3c384a586ddf1cc496acab7e6bc2d9438a
sv0ne <serjvolk222@gmail.com>
2021-09-04 20:51:38 +0300

    51 - React JS практика - Users API

--- projects/01/my-app/src/components/Users/Users.jsx
+++ projects/01/my-app/src/components/Users/Users.jsx
@@ -1,17 +1,22 @@
 import css from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/no-user.png';
 
 let Users = (props) => {
 
     console.log(props.users.length);
     if(props.users.length === 0){
-        props.setUsers([
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items);
+        });
+        /*props.setUsers([
             {id: 1, photoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg",
                 followed: true, fullName: 'John', status: 'I am a boss', location: {city: 'Minsk', country: 'Belarus'}},
             {id: 2, photoUrl: "https://img.championat.com/news/big/s/e/v-35-sheva-jagodka-opjat_1317288612541958436stah.jpg",
                 followed: false, fullName: 'Sasha', status: 'I am a boss to', location: {city: 'Moscow', country: 'Russia'}},
             {id: 3, photoUrl: "https://icdn.lenta.ru/images/2021/08/31/12/20210831120729192/square_320_a634eefb17c278a65869d86b40e1af8d.jpeg",
                 followed: true, fullName: 'Andrew', status: 'I am iron man', location: {city: 'Kiev', country: 'Ukraine'}}
-        ]);
+        ]);*/
     }
 
     return  (
@@ -20,7 +25,7 @@ let Users = (props) => {
                 props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photoUrl} alt="" className={css.userPhoto}/>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" className={css.userPhoto}/>
                         </div>
                         <div>
                             {u.followed ?
@@ -34,8 +39,8 @@ let Users = (props) => {
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>u.location.country</div>
+                            <div>u.location.city</div>
                         </span>
                     </span>
                 </div>)
