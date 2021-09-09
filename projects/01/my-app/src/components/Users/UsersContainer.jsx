4439eccb9b6a223077f48df0fc839e2576f600f7
sv0ne <serjvolk222@gmail.com>
2021-09-09 20:56:13 +0300

    56 - React JS практика - Презентационная и Контейнерная компоненты

--- projects/01/my-app/src/components/Users/UsersContainer.jsx
+++ projects/01/my-app/src/components/Users/UsersContainer.jsx
@@ -1,6 +1,36 @@
-import Users from "./Users";
 import {connect} from "react-redux";
 import {followAC, setUsersAC, unfollowAC, setCurrentPageAC, setTotalUsersCountAC} from "../../redux/users-reducer";
+import React from "react";
+import * as axios from "axios";
+import Users from "./Users";
+
+class UsersContainer extends React.Component{
+    componentDidMount() { // Метод жизненного цикла. Вызывается сразу после монтирования
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            console.log(response);
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+        });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+        });
+    }
+
+    render () {
+        return <Users totalUsersCount={this.props.totalUsersCount}
+                      pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      onPageChanged={this.onPageChanged}
+                      users={this.props.users}
+                      follow={this.props.follow}
+                      unfollow={this.props.unfollow}
+        />
+    }
+}
 
 let mapStateToProps = (state) => { // Принимает весь state и отдает только то что нужно
     return {
@@ -31,4 +61,4 @@ let mapDispatchToProps = (dispatch) => { // Отдаем колбеки кото
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
