c4d5d150f00296c500db213ddd3f40ba1250c683
sv0ne <serjvolk222@gmail.com>
2021-10-02 17:24:48 +0300

    75 - React JS - redux-form введение (login)

--- projects/01/my-app/src/components/Login/Login.jsx
+++ projects/01/my-app/src/components/Login/Login.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import css from "./Login.module.css";
+import {Field, reduxForm} from "redux-form";
+const LoginForm = (props) => {
+    return <form onSubmit={props.handleSubmit} className={css.form}>
+        <div>
+            <Field component={"input"} name={"login"} placeholder={"Login"}/>
+        </div>
+        <div>
+            <Field component={"input"} name={"password"} placeholder={"Password"}/>
+        </div>
+        <div>
+            <Field component={"input"} name={"rememberMe"} type={"checkbox"} className={css.checkbox}/> Remember me
+        </div>
+        <div>
+            <button>Login</button>
+        </div>
+    </form>
+}
+
+const LoginReduxForm = reduxForm({
+    form: 'login' // Должно быть уникальное имя
+})(LoginForm);
 
 const Login = (props) => {
-    return <h1>Login</h1>
+
+    const onSubmit = (formData) => {
+        console.log(formData);
+    }
+
+    return <div className={css.login}>
+        <h1>Login</h1>
+        <LoginReduxForm onSubmit={onSubmit}/>
+    </div>
 }
 
 export default Login;
--- projects/01/my-app/src/redux/redux-store.js
+++ projects/01/my-app/src/redux/redux-store.js
@@ -5,13 +5,15 @@ import dialogsReducer from "./dialogs-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
+import {reducer as formReducer} from "redux-form";
 
 // Комбайним редьюсеры. Собираем их в кучу.
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
 
 // Отдаем комбайн с редьюсерами стору
