ae29c9b37c39a5e3610e1994ee64280436d46a23
sv0ne <serjvolk222@gmail.com>
2021-11-03 21:48:08 +0200

    94 - React.lazy и React.Suspense, зачем? #94 - ReactJS

--- projects/01/my-app/src/App.js
+++ projects/01/my-app/src/App.js
@@ -1,12 +1,14 @@
+import React from "react";
 import './App.css';
 import {BrowserRouter, Route, withRouter} from "react-router-dom";
 import Technologies from './components/Technologies/Technologies';
 import Comments from "./components/Comments/Comments";
 import Nav from "./components/Nav/Nav";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
+//import DialogsContainer from "./components/Dialogs/DialogsContainer";
+//import ProfileContainer from "./components/Profile/ProfileContainer";
+
 import Login from "./components/Login/Login";
 import {Component} from "react";
 import {connect, Provider} from "react-redux";
@@ -14,6 +16,10 @@ import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
 import store from './redux/redux-store';
 import {compose} from "redux";
+import {withSuspense} from "./hoc/withSuspense";
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 class App extends Component {
     componentDidMount() {
@@ -32,10 +38,23 @@ class App extends Component {
                             <Nav/>
                         </div>
                         <div className="main__content">
-                            <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                            { // Так было до того как мы обернули в хок withSuspense
+                                                            /*<Route path='/profile/:userId?' render={() => {
+                                                                return <React.Suspense fallback={<div>Loading...</div>}>
+                                                                    <ProfileContainer/>
+                                                                </React.Suspense>
+                                                            }}/>
+                                                            <Route path='/dialogs' render={() => {
+                                                                return <React.Suspense fallback={<div>Loading...</div>}>
+                                                                    <DialogsContainer/>
+                                                                </React.Suspense>
+                                                            }}/>*/
+                            }
+                            <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)}/>
+                            <Route path='/dialogs' render={withSuspense(DialogsContainer)}/>
+
                             <Route path='/technologies' render={() => <Technologies/>}/>
                             <Route path='/comments' render={() => <Comments/>}/>
-                            <Route path='/dialogs' render={() => <DialogsContainer/>}/>
                             <Route path='/users' render={() => <UsersContainer/>}/>
                             <Route path='/login' render={() => <Login/>}/>
                         </div>
